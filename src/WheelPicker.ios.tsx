8da1e156e56969ab0142f87b9db0a14bd478275f
Pedro Fuentes <pedro@vantageio.com>
2022-06-09 22:22:07 -0400

    improve WheelPicker ios

--- src/WheelPicker.ios.tsx
+++ src/WheelPicker.ios.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { View } from "react-native";
-import { Picker } from "@react-native-picker/picker";
+import { Picker, PickerProps } from "@react-native-picker/picker";
 
 interface Props {
   data: Array<string>;
@@ -9,7 +9,7 @@ interface Props {
   disabled?: boolean;
 }
 
-const WheelPicker: React.FC<Props> = (props) => {
+function WheelPicker<T>(props: Props & PickerProps<T>) {
   const [selectedItem, setSelectedItem] = useState(props.selectedItem || 0);
   const { data, onItemSelected, disabled } = props;
   if (!data || data.length === 0) return null;
@@ -18,17 +18,17 @@ const WheelPicker: React.FC<Props> = (props) => {
       <Picker
         {...props}
         selectedValue={data[selectedItem]}
-        onValueChange={(value, index): void => {
+        onValueChange={(_, index): void => {
           if (onItemSelected) onItemSelected(index);
           setSelectedItem(index);
         }}
       >
         {data.map((i, index) => (
-          <Picker.Item key={index} label={i} value={i} />
+          <Picker.Item {...props} key={index} label={i} value={i} />
         ))}
       </Picker>
     </View>
   );
-};
+}
 
 export default WheelPicker;
